86bcbde5c08f56a18de01fb2f151d04d37d3d96c
Vraj Patel <77250162+vpatel78@users.noreply.github.com>
2021-04-21 16:36:30 -0400

    Update Question 2.c

--- Question 2.c
+++ Question 2.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <time.h>
 
-void my_func_with_only_subscript();
-void my_func_with_pointers();
+void subscript();
+void pointers();
 
 int main()
 {
@@ -14,7 +14,7 @@ clock_t start1, end1;
 start = clock();
   
 for(int i=0; i<100; i++){
-    my_func_with_only_subscript();   
+    subscript();   
 }
   
 // closing the cpu clock.
@@ -26,7 +26,7 @@ printf("Time taken with subscript: %ld\n", (end - start));
 start1 = clock();
   
 for(int i=0; i< 100;  i++){
-    my_func_with_pointers();
+    pointers();
 }
   
 // closing the cpu clock.
@@ -38,7 +38,7 @@ printf("Time taken with pointers: %ld\n", (end1 - start1));
 }
 
 // function to reference the 2D array through subscripts.
-void my_func_with_only_subscript(){ //assuming that the usage of
+void subscript(){ //assuming that the usage of
 
 int arr[10][10] = {0}; //square matrice is not mandatory
 
@@ -51,7 +51,7 @@ for(int j=0; j<10 ; j++){
 }
 
 // function to reference the 2D array through pointers.
-void my_func_with_pointers() { //assuming that the usage of
+void pointers() { //assuming that the usage of
 
 int arr[10][10] = {0}; //square matrice is not mandatory
 
